37be740baf989e078c97c7634b3030920796bf07
James Andrews <jamesftw@Jamess-MacBook-Pro.local>
2017-01-15 15:15:57 -0800

    fixed index

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(app.get('port'))
