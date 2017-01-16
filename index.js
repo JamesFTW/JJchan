07abc7860331cbe99839a200100d2755ca4c8f47
James Andrews <jamesftw@Jamess-MacBook-Pro.local>
2017-01-15 17:53:37 -0800

    added a bunch of stuff

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express')
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
 var app = express()
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set('port', (process.env.PORT || 5000));
 app.set('db',   (process.env.MONGODB_URI || 'mongodb://heroku_66t99vts:jh4m8juf98hunt21k23go7ap1v@ds111589.mlab.com:11589/heroku_66t99vts'))
@@ -7,30 +13,29 @@ app.set('db',   (process.env.MONGODB_URI || 'mongodb://heroku_66t99vts:jh4m8juf9
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
+app.get('/', function (req, res) {
+  MongoClient.connect(app.get('db'), function(err, db) {
+    db.collection('Note').find().toArray(function(err, docs) {
+      var results = ''
+      for (var i = 0; i < docs.length; i++){
+        results += (" '" + docs[i].body + "' - " +  docs[i].username + "\n\n")
+      }
+      res.send('<pre>' + results+ '</pre>')
+      db.close()
+    });
+  })
+})
 
-// Use connect method to connect to the server
-MongoClient.connect(app.get('db'), function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-      app.get('/', function (req, res) {
-        res.send('Hello World!')
-      })
-
-      app.post('/new', function(req, res){
-        var col = db.collection('Note');
-        col.insertOne({
-          body: req.param('body'),
-          username: req.param('username')
-        }, function(err, r) {
-          if (err) throw err;
-          console.log('We posted something to the database!')
-          // Finish up test
-          db.close();
-        });
-      })
-  db.close();
-});
+app.post('/new', function(req, res) {
+  MongoClient.connect(app.get('db'), function(err, db) {
+    var col = db.collection('Note');
+    col.insertOne(req.body, function(err, r) {
+      if (err) throw err;
+      db.close()
+      res.send('Success!')
+    });
+  })
+})
 
 
 
